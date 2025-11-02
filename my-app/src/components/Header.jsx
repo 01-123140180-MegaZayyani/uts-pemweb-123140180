119a9794c387bbd65dfaed7759c1f42c807af92c
01-123140180-MegaZayyani <mega.123140180@student.itera.ac.id>
2025-11-02 23:36:26 +0700

    membuat header

---
+++ my-app/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="app-header">
+      <h1>🎬 Movie Database Explorer</h1>
+      <p>Aplikasi pencarian dan katalog film menggunakan OMDb API.</p>
+    </header>
+  );
+}
+
+export default Header;
