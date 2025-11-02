13a3a8933cd46fd02dd42700adc29a90340fc366
01-123140180-MegaZayyani <mega.123140180@student.itera.ac.id>
2025-11-03 01:55:47 +0700

    memperbaiki alamat import

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 // Import ReactDOM Client untuk me-render aplikasi ke DOM
 import ReactDOM from 'react-dom/client'; 
 import App from './App.jsx'; // Mengimpor komponen utama
-import './index.css'; // Opsional: Untuk styling reset atau global body
+import './App.css';
 
 // Mendapatkan elemen root dari index.html
 const rootElement = document.getElementById('root');
